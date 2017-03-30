9b5f563b699cc21feee9212e11127036ce5826bc
Deconinck Damien <ddeconinck@auchan.net>
2017-03-30 19:30:42 +0200

    Add users management in administration

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -3,7 +3,9 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
+use AppBundle\Form\RegisterType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -14,15 +16,14 @@ class AdminController extends Controller
 {
     /**
      * @Route("/users", name="users")
+     * @Template
      */
     public function usersAction()
     {
         $users = $this->getDoctrine()->getRepository(User::class)
             ->findBy([], ['username' => 'ASC']);
 
-        return $this->render('AppBundle:Admin:users.html.twig', [
-            'users' => $users
-        ]);
+        return ['users' => $users];
     }
 
     /**
@@ -37,14 +38,42 @@ class AdminController extends Controller
         $manager->remove($user);
         $manager->flush();
 
+        $this->get('session')->getFlashBag()->add(
+            'success',
+            "Le compte a bien été supprimé."
+        );
+
         return $this->redirectToRoute('users');
     }
 
     /**
      * @Route("/add-user", name="add-user")
+     * @Template
      */
     public function addUserAction(Request $request)
     {
-        return $this->render('AppBundle:Admin:add_user.html.twig');
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add(
+                'success',
+                "Le compte a bien été créé."
+            );
+
+            return $this->redirect($this->generateUrl('users'));
+        }
+
+        return ['form' => $form->createView()];
     }
 }
