59a7851ddb33308c9204522b355a34ea07fa2a2e
Bonini Raphael <raphael.eros.bonini@gmail.com>
2017-04-09 03:00:47 +0200

    optimisation of expertise form (first part)

---
+++ src/AppBundle/Form/ExpertiseType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ExpertiseType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder->add('interventions', CollectionType::class, array(
+            'entry_type'   => InterventionType::class,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_expertise';
+    }
+
+}
