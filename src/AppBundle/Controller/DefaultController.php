2e7eb4cdde91de385805fb7af9a042dc44cd2733
Bonini Raphael <raphael.eros.bonini@gmail.com>
2017-04-18 00:12:15 +0200

    user interface implementation

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\SearchVehicleType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,6 +14,36 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('AppBundle:Home:index.html.twig');
+        $searchForm = $this->createForm(SearchVehicleType::class);
+
+        $searchForm->handleRequest($request);
+
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+
+            $search = $searchForm->getData();
+
+            $vehicle = $this->getDoctrine()
+                ->getRepository('AppBundle:Vehicle')
+                ->findOneBy(array('registration' => $search['registration'], 'frame' => $search['frame']));
+
+            if (!$vehicle) {
+                $this->addFlash(
+                    'notice',
+                    'Le véhicule que vous recherchez n\'existe pas'
+                );
+            }
+
+
+            return $this->render('AppBundle:Home:index.html.twig', array(
+                'form' => $searchForm->createView(),
+                'vehicle' => $vehicle,
+            ));
+        }
+
+        return $this->render('AppBundle:Home:index.html.twig', array(
+            'form' => $searchForm->createView(),
+            'vehicle' => null,
+        ));
     }
+
 }
---
+++ src/AppBundle/Form/SearchVehicleType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class SearchVehicleType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('registration', TextType::class, array('label' => 'Immatriculation'))
+            ->add('frame', TextType::class, array('label' => 'N° châssis'))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_searchvehicle';
+    }
+
+
+}
